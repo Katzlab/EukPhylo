feab503fb6671a4df4e023e4f948a239bb42f09a
Adri K. Grow <42044618+adriannagrow@users.noreply.github.com>
2025-02-13 18:16:29 -0500

    Update run_eukphylo.sh

--- PTL2/run_eukphylo.sh
+++ PTL2/run_eukphylo.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 ## Last updated Jan 2025 by Auden Cote-L'Heureux; modified Feb 2025 by Adri K. Grow
 
 ## This shell script is used for running EukPhylo part 2, and includes a general setup for use on an HPC that uses 
@@ -5,8 +6,6 @@
 ## EukPhylo Wiki (https://github.com/Katzlab/EukPhylo/wiki/EukPhylo-Part-2:-MSAs,-trees,-and-contamination-loop).
 ## These run commands can also be copied and run in the terminal / command line separately, without a shell script.
 
-
-#!/bin/bash ## DELETE all lines before this to run
 
 ## SLURM-SPECIFIC SETUP BELOW
 
