e3d215c7e9bfb35a1fa43fde56691a208b1e16e2
Adri K. Grow <42044618+adriannagrow@users.noreply.github.com>
2025-02-13 18:22:03 -0500

    Update wrapper_submit.sh

--- PTL1/Transcriptomes/Scripts/wrapper_submit.sh
+++ PTL1/Transcriptomes/Scripts/wrapper_submit.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 ## Last updated Jan 2025 by Auden Cote-L'Heureux
 
 ## This script is intended to be used to process genomic CDS with EukPhylo part 1 on an HPC that uses the Slurm workload manager.
@@ -6,15 +7,13 @@
 ## here: https://github.com/Katzlab/EukPhylo/wiki/EukPhylo-Part-1:-GF-assignment
 
 
-#!/bin/bash
-
 ## Slurm specific code
 
 #SBATCH --job-name=EukPhylo
 #SBATCH --output=EukPhylo.%j.out # Stdout (%j expands to jobId)
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --ntasks-per-node=64 # #change to number of srun when running multiple instances
+#SBATCH --ntasks-per-node=1 # #change to number of srun when running multiple instances
 #SBATCH --mem=160G
 
 module purge       #Cleans up any loaded modules
@@ -30,7 +29,7 @@ parent='/Your/Home/Folder'
 ## Example commands
 
 # A simple run that goes from script 1 to script 7 (the last script) using the Universal genetic code
-srun -D ${parent}Scripts python3 ${parent}Scripts/wrapper.py --first_script 1 --last_script 7 --assembled_transcripts ${parent}AssembledTranscripts --genetic_code ${parent}Gcode.txt --databases ${parent}Databases > log.txt
+srun -D ${parent}Scripts python3 ${parent}Scripts/wrapper.py --first_script 1 --last_script 7 --assembled_transcripts ${parent}AssembledTranscripts --genetic_code ${parent}Gcode.txt --databases ${parent}Databases > log.out
 
 # Including the cross-plate contamination step, using conspecific names
-srun -D ${parent} python3 ${parent}Scripts/wrapper.py --first_script 1 --last_script 7 --assembled_transcripts ${parent}AssembledTranscripts --output . --genetic_code ${parent}Gcode.txt --databases ${parent}Databases --xplate_contam --conspecific_names ${parent}Conspecific.txt > log.txt
+srun -D ${parent} python3 ${parent}Scripts/wrapper.py --first_script 1 --last_script 7 --assembled_transcripts ${parent}AssembledTranscripts --output . --genetic_code ${parent}Gcode.txt --databases ${parent}Databases --xplate_contam --conspecific_names ${parent}Conspecific.txt > log.out
