6924789f0faba6a56d6da3eafd45ca778e868868
Godwin Ani <aniigodwinn@gmail.com>
2025-02-24 11:27:06 -0500

    docker wrapper_submit.sh

--- PTL1/Genomes/Scripts/wrapper_submit.sh
+++ PTL1/Genomes/Scripts/wrapper_submit.sh
@@ -6,27 +6,9 @@
 ## needs and restrictions, followed by some example commands taken from the GitHub Wiki, more detail for which can be found
 ## here: https://github.com/Katzlab/EukPhylo/wiki/EukPhylo-Part-1:-GF-assignment
 
-## Slurm specific code
-
-#SBATCH --job-name=EukPhylo
-#SBATCH --output=EukPhylo.%j.out # Stdout (%j expands to jobId)
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --ntasks-per-node=64 # #change to number of srun when running multiple instances
-#SBATCH --mem=160G
-
-module purge       #Cleans up any loaded modules
-module load slurm
-module load tqdm/4.62.3-GCCcore-11.2.0
-module load Biopython/1.79-foss-2021b
-module load BLAST+/2.12.0-gompi-2021b
-module load DIAMOND/2.0.13-GCC-11.2.0
-module load VSEARCH/2.22.1-GCC-11.3.0
-
-parent='/Your/Home/Folder'
 
 ## Example run command
 
 # Start at script 1 and go through script 5 (the final script) using the Universal genetic code
-srun -D ${parent}Scripts python3 ${parent}Scripts/wrapper.py -1 1 -2 5 --cds ${parent}Input -o ${parent}Output --genetic_code Universal --databases ${parent}Databases > log.out
+python3 Scripts/wrapper.py -1 1 -2 5 --cds Input -o Output --genetic_code Universal --databases Databases > log.out
 
