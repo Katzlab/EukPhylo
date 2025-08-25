e7855329219a3f05209977f77a657428cb602977
Adri K. Grow <42044618+adriannagrow@users.noreply.github.com>
2025-08-25 11:42:05 -0400

    Update wrapper_submit.sh

--- PTL1/Transcriptomes/Scripts/wrapper_submit.sh
+++ PTL1/Transcriptomes/Scripts/wrapper_submit.sh
@@ -6,33 +6,40 @@
 ## needs and restrictions, followed by some example commands taken from the GitHub Wiki, more detail for which can be found
 ## here: https://github.com/Katzlab/EukPhylo/wiki/EukPhylo-Part-1:-GF-assignment
 
-
+############### FOR SMITH GRID HPC ###############
 ## Slurm specific code
-
 #SBATCH --job-name=EukPhylo
 #SBATCH --output=EukPhylo.%j.out # Stdout (%j expands to jobId)
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --ntasks-per-node=1 # #change to number of srun when running multiple instances
+#SBATCH --ntasks-per-node=1 ##change to number of srun when running multiple instances
 #SBATCH --mem=160G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=email@xxx.edu ##add your email address for job updates
-
 module purge       #Cleans up any loaded modules
-#Unity server
-module use /gridapps/modules/all
-module load conda/latest
-conda activate hook_env
-module load uri/main
-module load diamond/2.1.7
-module load VSEARCH/2.22.1-GCC-11.3.0
-
-#Grid server
 module load slurm
 module load tqdm/4.62.3-GCCcore-11.2.0
 module load Biopython/1.79-foss-2021b
 module load BLAST+/2.12.0-gompi-2021b
 module load DIAMOND/2.0.13-GCC-11.2.0
+module load VSEARCH/2.22.1-GCC-11.3.0
+
+############### FOR UMASS UNITY HPC ###############
+## Slurm specific code
+#SBATCH --job-name=EukPhylo
+#SBATCH --output=EukPhylo.%j.out # Stdout (%j expands to jobId)
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=64
+#SBATCH --mem=40G
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=email@xxx.edu
+module purge       #Cleans up any loaded modules
+module use /gridapps/modules/all
+module load conda/latest
+conda activate hook_env
+module load uri/main
+module load diamond/2.1.7
 module load VSEARCH/2.22.1-GCC-11.3.0
 
 parent='/Your/Home/Folder/'
